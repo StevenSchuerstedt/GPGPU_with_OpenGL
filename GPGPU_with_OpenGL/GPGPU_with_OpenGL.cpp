065799fbf4dba247973eb732d115eaa1c13ecb8a
Steven Schürstedt <steven@schuerstedt.com>
2021-04-05 19:24:23 +0200

    more setup

--- GPGPU_with_OpenGL/GPGPU_with_OpenGL.cpp
+++ GPGPU_with_OpenGL/GPGPU_with_OpenGL.cpp
@@ -3,10 +3,50 @@
 
 #include "GPGPU_with_OpenGL.h"
 
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
 using namespace std;
+
+
+GLFWwindow* window;
+
+int width = 1024;
+int height = 512;
+
+void init() {
+
+	glfwInit();
+
+	window = glfwCreateWindow(width, height, "Opengl", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = true;
+	glewInit();
+
+}
+
+void render() {
+
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+}
 
 int main()
 {
+
+	init();
+	render();
+
+
 	cout << "Hello CMake." << endl;
 	return 0;
 }
