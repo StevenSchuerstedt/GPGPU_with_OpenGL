d3411bb5b93ed7cf715aa4477eac8af2be5cf492
Steven Schürstedt <steven@schuerstedt.com>
2021-04-05 18:53:47 +0200

    Projektdateien hinzufügen.

---
+++ GPGPU_with_OpenGL/GPGPU_with_OpenGL.cpp
@@ -0,0 +1,12 @@
+﻿// GPGPU_with_OpenGL.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "GPGPU_with_OpenGL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
